092294803a1061a58eeabe2308ebd66e69d25c37
aspalding89 <aspalding89@icloud.com>
2021-07-15 14:40:45 +1000

    Added campground routes

--- app.js
+++ app.js
@@ -1,115 +1,75 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const catchAsync = require('./utilities/catchAsync');
-const expressError = require('./utilities/expressError');
-const ejsMate = require('ejs-mate');
-const { campgroundSchema, reviewSchema } = require('./schemas')
-const methodOverride = require('method-override');
-const mongoose = require('mongoose');
-const Campground = require('./models/campground');
-const Review = require('./models/reviews')
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const { campgroundSchema, reviewSchema } = require("./schemas");
+const methodOverride = require("method-override");
+const expressError = require('./utilities/expressError')
+const catchAsync = require('./utilities/catchAsync')
+const Review = require("./models/reviews");
+const Campground = require('./models/campground')
+const campgrounds = require("./routes/campgrounds");
 
 mongoose.connect(`mongodb://localhost:27017/yelp-camp`, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
 });
 
-mongoose.connection.on('error', console.error.bind(console, 'connection error'));
-mongoose.connection.once('open', () => {
-});
+mongoose.connection.on("error", console.error.bind(console, "connection error"));
+mongoose.connection.once("open", () => { });
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const app = express();
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(',')
-    throw new expressError(msg, 400)
-  } else {
-    next();
-  }
-}
+
 
 const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map(el => el.message).join(',')
-    throw new expressError(msg, 400)
-  } else {
-    next();
-  }
-}
-
-app.get('/', (req, res) => {
-  res.render('home');
+	const { error } = reviewSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map(el => el.message).join(",");
+		throw new expressError(msg, 400);
+	} else {
+		next();
+	}
+};
+app.use("/campgrounds", campgrounds);
+app.get("/", (req, res) => {
+	res.render("home");
 });
 
-app.get('/campgrounds', catchAsync(async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render('campgrounds/index', { campgrounds });
-}));
+app.post(
+	"/campgrounds/:id/reviews", validateReview, catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		const review = new Review(req.body.review);
+		campground.reviews.push(review);
+		await review.save();
+		await campground.save();
+		res.redirect(`/campgrounds/${campground._id}`);
+	})
+);
 
-app.get('/campgrounds/new', (req, res) => {
-  res.render('campgrounds/new');
+app.delete(
+	"/campgrounds/:id/reviews/:reviewId", catchAsync(async (req, res) => {
+		const { id, reviewId } = req.params;
+		await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+		await Review.findByIdAndDelete(reviewId);
+		res.redirect(`/campgrounds/${id}`);
+	})
+);
+
+app.all("*", (req, res, next) => {
+	next(new expressError("Page not found", 401));
 });
-
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
-  const campground = new Campground(req.body.campground);
-  await campground.save();
-  res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-  const campground = await Campground.findById(req.params.id).populate('reviews');
-  res.render('campgrounds/show', { campground });
-}));
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-  const campground = await Campground.findById(req.params.id)
-  res.render('campgrounds/edit', { campground });
-}));
-
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-  res.redirect(`/campgrounds/${campground._id}`);
-}));
-
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect('/campgrounds');
-}));
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  const review = new Review(req.body.review);
-  campground.reviews.push(review);
-  await review.save();
-  await campground.save();
-  res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-  const { id, reviewId } = req.params;
-  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-  await Review.findByIdAndDelete(reviewId)
-  res.redirect(`/campgrounds/${id}`)
-}))
-
-app.all('*', (req, res, next) => {
-  next(new expressError('Page not found', 401));
-})
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500 } = err;
-  if (!err.message) err.message = "Oh no, something went wrong";
-  res.status(statusCode).render('error', { err });
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = "Oh no, something went wrong";
+	res.status(statusCode).render("error", { err });
 });
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(3000, () => console.log("Server started on port 3000"));
